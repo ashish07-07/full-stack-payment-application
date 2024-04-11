d42b229729811a72ca2cdba83041bc82906ec611
B <bkashishh07@gmail.com>
2024-04-12 02:35:48 +0530

    changes

--- wallet/paytm-project-starter-monorepo-migration/apps/bank-webhook/src/index.ts
+++ wallet/paytm-project-starter-monorepo-migration/apps/bank-webhook/src/index.ts
@@ -1,6 +1,27 @@
 import express from "express";
-import { db } from "@repo/db/client";
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/", function (req, res) {});
+app.post("/", function (req, res) {
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+  db.balance.update({
+    where: {
+      userId: paymentInformation.userId,
+    },
+    data: {
+      amount: {
+        increment: paymentInformation.amount,
+      },
+    },
+  });
+});
+db.onRampTransaction.update({
+  where: {
+    token,
+  },
+});
